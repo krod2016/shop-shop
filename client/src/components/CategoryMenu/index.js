69f12d39424215c07312cc53901d0e2b48cfdf44
krod2016 <katrinajrodriguez1993@gmail.com>
2022-02-14 18:31:40 -0600

    added tests

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_CATEGORIES } from '../../utils/queries';
+import React, { useEffect } from 'react';
+import { useQuery } from "@apollo/client";
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import { useStoreContext } from "../../utils/globalstate";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY, UPDATE_PRODUCTS } from '../../utils/actions';
 
 function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+  const [state, dispatch] = useStoreContext();
+const { id } = useParams();
+
+const [currentProduct, setCurrentProduct] = useState({})
+
+const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+const { products } = state;
+
+useEffect(() => {
+  if (products.length) {
+    setCurrentProduct(products.find(product => product._id === id));
+  } else if (data) {
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: data.products
+    });
+  }
+}, [products, data, dispatch, id]);
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map((item) => (
+      {categories.map(item => (
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
         </button>
       ))}
     </div>
-  );
+  );  
 }
 
 export default CategoryMenu;
